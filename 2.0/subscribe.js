ff19c4026aa1d7e9baaa648381d77acc1cdd4e79
wuhaoran <wuhaoran@kaikeba.com>
2021-07-30 19:31:30 +0800

    feat: add subscribe

--- 2.0/subscribe.js
+++ 2.0/subscribe.js
@@ -1,2 +1,25 @@
 // 发布订阅模式
 
+class Event {
+  _list = [];
+  on(key, fn) {
+    this._list.push({ key, fn });
+  }
+  emit(key, data) {
+    const cb = this._list.find((item) => item.key === key);
+    cb && cb.fn(data);
+  }
+}
+
+const e = new Event();
+
+e.on("first", (data) => {
+  console.log(`${data}`);
+});
+e.on("second", (data) => {
+  console.log(`${data}`);
+});
+
+e.emit("first", "发送第一个数据");
+e.emit("second", "发送第二个数据");
+e.emit("first", "发送第一个数据");
